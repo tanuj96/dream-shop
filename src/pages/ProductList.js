8d6e73948166d1e1c9a94bec4b45743a990d8c70
tanuj96 <tanujmittal96@gmail.com>
2023-12-17 00:48:18 +0530

    product list page rating filter ui

--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -5,10 +5,23 @@ import { Link } from "react-router-dom";
 
 const ProductList = () => {
   return (
-    <div className="products-list">
-      {Products.map((product) => {
-        return <Link key={product.id}  to={`/productDetails/${product.id}`}><ProductCard product={product}/></Link>;
-      })}
+    <div className="listing-page-container">
+      <div className="product-filter-container">
+        <h5>Customer Ratings</h5>
+        <ul>
+          <li><input type="checkbox"/>4 above</li>
+          <li><input type="checkbox"/>3 above</li>
+        </ul>
+      </div>
+      <div className="products-list">
+        {Products.map((product) => {
+          return (
+            <Link key={product.id} to={`/productDetails/${product.id}`}>
+              <ProductCard product={product} />
+            </Link>
+          );
+        })}
+      </div>
     </div>
   );
 };
