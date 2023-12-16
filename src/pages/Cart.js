e9dafa46d64faf3ec2e03f684eeea92e18a1b0b4
tanuj96 <tanujmittal96@gmail.com>
2023-12-17 00:12:22 +0530

    cart page implementation , added cart products to local storage,added product routes from cart page product cards , cart page css refactor

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Cart = () => {
-  return (
-    <div>Cart</div>
-  )
-}
+  const [cartProducts, setCartProducts] = useState([]);
 
-export default Cart
+  useState(() => {
+    const storedCartProducts = JSON.parse(localStorage.getItem("cart"));
+    setCartProducts(storedCartProducts);
+  });
+
+  return (
+    <div className="product-cart-container">
+      {cartProducts.map((product) => {
+        return (
+          <div className="cart-card">
+            <Link to={`/productDetails/${product.id}`}>
+              <div className="cart-product-image-container">
+                <img
+                  src={product.image}
+                  alt="product-view"
+                  className="cart-product-image"
+                />
+              </div>
+            </Link>
+            <div className="cart-product-details-container">
+              <Link to={`/productDetails/${product.id}`}>
+                <h2>{product.title}</h2>
+              </Link>
+              <h3>₹{product.price}</h3>
+              <p>quantity</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Cart;
--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,30 +1,50 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import { Products } from "../utils/products";
 
 const ProductDetail = () => {
-  const {productId} = useParams();
-  const [selectedProduct , setSelectedProduct] = useState(null);
+  const { productId } = useParams();
+  const [selectedProduct, setSelectedProduct] = useState(null);
 
-  useEffect(()=>{
-    const matchProduct = Products.find(item => item.id === parseInt(productId));
+  useEffect(() => {
+    const matchProduct = Products.find(
+      (item) => item.id === parseInt(productId)
+    );
     setSelectedProduct(matchProduct);
-  },[productId]);
+  }, [productId]);
+
+  const addToCart = () => {
+    if (localStorage.getItem("cart")) {
+      let storedArrayJsonString = localStorage.getItem("cart");
+      let storedArray = JSON.parse(storedArrayJsonString);
+      let cart = JSON.stringify([selectedProduct, ...storedArray]);
+      localStorage.setItem("cart", cart);
+    } else {
+      let cart = JSON.stringify([selectedProduct]);
+      localStorage.setItem("cart", cart);
+    }
+  };
 
   return (
-    <div className='product-details'>
-      <div className='product-picture'>
-        <img alt='productView' className='product-details-image' src={selectedProduct?.image}/>
+    <div className="product-details">
+      <div className="product-picture">
+        <img
+          alt="productView"
+          className="product-details-image"
+          src={selectedProduct?.image}
+        />
       </div>
-      <div className='product-information'>
+      <div className="product-information">
         <h2>{selectedProduct?.title}</h2>
         <h2>₹{selectedProduct?.price}</h2>
         <p>{selectedProduct?.rating?.rate}</p>
         <p>{selectedProduct?.description}</p>
-        <button className='cart-btn'>Add To Cart</button>
+        <button className="cart-btn" onClick={addToCart}>
+          Add To Cart
+        </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductDetail
+export default ProductDetail;
