2d7bf8b08457a7244756e9af384c66dd0e8417c1
tanuj96 <tanujmittal96@gmail.com>
2023-12-16 19:55:22 +0530

    fetch product id using useParams , made product details dynamic

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+import { Products } from "../utils/products";
 
 const ProductDetail = () => {
+  const {productId} = useParams();
+  const [selectedProduct , setSelectedProduct] = useState(null);
+
+  useEffect(()=>{
+    const matchProduct = Products.find(item => item.id === parseInt(productId));
+    setSelectedProduct(matchProduct);
+  },[productId]);
+
   return (
     <div className='product-details'>
       <div className='product-picture'>
-        <img className='product-details-image' src='https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg' alt='product-photo'/>
+        <img className='product-details-image' src={selectedProduct?.image} alt='product-photo'/>
       </div>
       <div className='product-information'>
-        <h2>Mens Casual Slim Fit</h2>
-        <h2>15.99</h2>
-        <p>Product Rating</p>
-        <h4>Product description</h4>
+        <h2>{selectedProduct?.title}</h2>
+        <h2>{selectedProduct?.price}</h2>
+        <p>{selectedProduct?.rating?.rate}</p>
+        <h4>{selectedProduct?.description}</h4>
         <button className='cart-btn'>Add To Cart</button>
       </div>
     </div>
