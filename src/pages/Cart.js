828f82b415398486c20315849bf706a9ab8bd792
tanuj96 <tanujmittal96@gmail.com>
2023-12-17 00:23:35 +0530

    fixed empty cart error break

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -11,28 +11,37 @@ const Cart = () => {
 
   return (
     <div className="product-cart-container">
-      {cartProducts.map((product) => {
-        return (
-          <div className="cart-card">
-            <Link to={`/productDetails/${product.id}`}>
-              <div className="cart-product-image-container">
-                <img
-                  src={product.image}
-                  alt="product-view"
-                  className="cart-product-image"
-                />
-              </div>
-            </Link>
-            <div className="cart-product-details-container">
+      {cartProducts ? (
+        cartProducts.map((product) => {
+          return (
+            <div className="cart-card">
               <Link to={`/productDetails/${product.id}`}>
-                <h2>{product.title}</h2>
+                <div className="cart-product-image-container">
+                  <img
+                    src={product.image}
+                    alt="product-view"
+                    className="cart-product-image"
+                  />
+                </div>
               </Link>
-              <h3>₹{product.price}</h3>
-              <p>quantity</p>
+              <div className="cart-product-details-container">
+                <Link to={`/productDetails/${product.id}`}>
+                  <h2>{product.title}</h2>
+                </Link>
+                <h3>₹{product.price}</h3>
+                <p>quantity</p>
+              </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })
+      ) : (
+        <div className="cart-empty-msg-container">
+        <p className="cart-empty-msg">
+          Your cart is empty! Explore our wide selection and find something you
+          like
+        </p>
+        </div>
+      )}
     </div>
   );
 };
